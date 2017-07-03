7541d54174fb4d49da9ecd49409378bde27805dd
Justin <loveless.justin89@Gmail.com>
2017-07-03 10:39:20 -0500

    Testing getting a list of builds using similar logic to the JenkinsAdapter

--- Plugins/BuildServerIntegration/TfsIntegration/TfsAdapter.cs
+++ Plugins/BuildServerIntegration/TfsIntegration/TfsAdapter.cs
@@ -11,6 +11,12 @@ using GitUIPluginInterfaces;
 using GitUIPluginInterfaces.BuildServerIntegration;
 using TfsInterop.Interface;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace TfsIntegration
 {
@@ -41,11 +47,12 @@ namespace TfsIntegration
     internal class TfsAdapter : IBuildServerAdapter
     {
         private IBuildServerWatcher _buildServerWatcher;
-        private ITfsHelper _tfsHelper;
         string _tfsServer;
+        private IList<Task<IEnumerable<string>>> _getBuildUrls;
         string _tfsTeamCollectionName;
         string _projectName;
         Regex _tfsBuildDefinitionNameFilter;
+        private HttpClient _httpClient;
 
         public void Initialize(IBuildServerWatcher buildServerWatcher, ISettingsSource config, Func<string, bool> isCommitInRevisionGrid)
         {
@@ -69,13 +76,22 @@ namespace TfsIntegration
                 && !string.IsNullOrEmpty(_tfsTeamCollectionName)
                 && !string.IsNullOrEmpty(_projectName))
             {
-                _tfsHelper = LoadAssemblyAndConnectToServer("TfsInterop.Vs2015")
-                    ?? LoadAssemblyAndConnectToServer("TfsInterop.Vs2013")
-                    ?? LoadAssemblyAndConnectToServer("TfsInterop.Vs2012");
+                var baseAdress = _tfsServer.Contains("://")
+                                     ? new Uri($"{_tfsServer}/{_tfsTeamCollectionName}/{_projectName}/_apis/build/builds?definitions=5&status8Filter=completed&top=1&api-version=2.0", UriKind.Absolute)
+                                     : new Uri(string.Format("{0}://{1}", Uri.UriSchemeHttp, _tfsServer), UriKind.Absolute);
 
-                if (_tfsHelper == null)
+                _httpClient = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true });
+                _httpClient.Timeout = TimeSpan.FromMinutes(2);
+                _httpClient.BaseAddress = baseAdress;
+
+                var buildServerCredentials = buildServerWatcher.GetBuildServerCredentials(this, true);
+
+                _getBuildUrls = new List<Task<IEnumerable<string>>>();
+
+                string[] projectUrls = _projectName.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var projectUrl in projectUrls.Select(s => baseAdress + "job/" + s.Trim() + "/"))
                 {
-                    Trace.WriteLine("fail to load the good interop assembly :(");
+                    AddGetBuildUrl(projectUrl);
                 }
             }
         }
@@ -123,8 +139,8 @@ namespace TfsIntegration
 
         public IObservable<BuildInfo> GetBuilds(IScheduler scheduler, DateTime? sinceDate = null, bool? running = null)
         {
-            if (_tfsHelper == null)
-                return Observable.Empty<BuildInfo>();
+            //if (_tfsHelper == null)
+            //    return Observable.Empty<BuildInfo>();
 
             return Observable.Create<BuildInfo>((observer, cancellationToken) =>
                 Task<IDisposable>.Factory.StartNew(
@@ -135,9 +151,45 @@ namespace TfsIntegration
         {
             try
             {
-                var builds = _tfsHelper.QueryBuilds(sinceDate, running);
+                if (_getBuildUrls.All(t => t.IsCanceled))
+                {
+                    observer.OnCompleted();
+                    return;
+                }
 
-                Parallel.ForEach(builds, detail => { observer.OnNext(CreateBuildInfo(detail)); });
+                foreach (var currentGetBuildUrls in _getBuildUrls)
+                {
+                    if (currentGetBuildUrls.IsFaulted)
+                    {
+                        Debug.Assert(currentGetBuildUrls.Exception != null);
+
+                        observer.OnError(currentGetBuildUrls.Exception);
+                        continue;
+                    }
+
+                    var buildContents = currentGetBuildUrls.Result
+                        .Select(buildUrl => GetResponseAsync(FormatToGetJson(buildUrl), cancellationToken).Result)
+                        .Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+                    foreach (var buildDetails in buildContents)
+                    {
+                        JObject buildDescription = JObject.Parse(buildDetails);
+                        var startDate = TimestampToDateTime(buildDescription["startTime"].ToObject<long>());
+                        var isRunning = buildDescription["building"].ToObject<bool>();
+
+                        if (sinceDate.HasValue && sinceDate.Value > startDate)
+                            continue;
+
+                        if (running.HasValue && running.Value != isRunning)
+                            continue;
+
+                        var buildInfo = CreateBuildInfo(buildDescription);
+                        if (buildInfo.CommitHashList.Any())
+                        {
+                            observer.OnNext(buildInfo);
+                        }
+                    }
+                }
             }
             catch (OperationCanceledException)
             {
@@ -149,28 +201,121 @@ namespace TfsIntegration
             }
         }
 
-        private static BuildInfo CreateBuildInfo(IBuild buildDetail)
+        private static BuildInfo CreateBuildInfo(JObject buildDetail)
         {
-            string sha = buildDetail.Revision.Substring(buildDetail.Revision.LastIndexOf(":") + 1);
+            return new BuildInfo() { Description = "Success", StartDate = DateTime.Now };
+        }
 
-            var buildInfo = new BuildInfo
+        private void AddGetBuildUrl(string projectUrl)
+        {
+            _getBuildUrls.Add(GetResponseAsync(FormatToGetJson(projectUrl), CancellationToken.None)
+                .ContinueWith(
+                    task =>
+                    {
+                        JObject jobDescription = JObject.Parse(task.Result);
+                        return jobDescription["builds"].Select(b => b["url"].ToObject<string>());
+                    },
+                    TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.AttachedToParent));
+        }
+
+        private string FormatToGetJson(string restServicePath)
+        {
+            if (!restServicePath.EndsWith("/"))
+                restServicePath += "/";
+            return restServicePath + "api/json";
+        }
+
+        private Task<Stream> GetStreamFromHttpResponseAsync(Task<HttpResponseMessage> task, string restServicePath, CancellationToken cancellationToken)
+        {
+#if !__MonoCS__
+            bool retry = task.IsCanceled && !cancellationToken.IsCancellationRequested;
+            bool unauthorized = task.Status == TaskStatus.RanToCompletion &&
+                                task.Result.StatusCode == HttpStatusCode.Unauthorized;
+
+            if (!retry)
             {
-                Id = buildDetail.Label,
-                StartDate = buildDetail.StartDate,
-                Status = (BuildInfo.BuildStatus)buildDetail.Status,
-                Description = buildDetail.Label + " (" + buildDetail.Description + ")",
-                CommitHashList = new[] { sha },
-                Url = buildDetail.Url,
-                ShowInBuildReportTab = false
-            };
+                if (task.Result.IsSuccessStatusCode)
+                {
+                    var httpContent = task.Result.Content;
 
-            return buildInfo;
+                    if (httpContent.Headers.ContentType.MediaType == "text/html")
+                    {
+                        // Jenkins responds with an HTML login page when guest access is denied.
+                        unauthorized = true;
+                    }
+                    else
+                    {
+                        return httpContent.ReadAsStreamAsync();
+                    }
+                }
+                else if (task.Result.StatusCode == HttpStatusCode.Forbidden)
+                {
+                    unauthorized = true;
+                }
+            }
+
+            if (retry)
+            {
+                return GetStreamAsync(restServicePath, cancellationToken);
+            }
+
+            if (unauthorized)
+            {
+                var buildServerCredentials = _buildServerWatcher.GetBuildServerCredentials(this, false);
+
+                if (buildServerCredentials != null)
+                {
+
+                    return GetStreamAsync(restServicePath, cancellationToken);
+                }
+
+                throw new OperationCanceledException(task.Result.ReasonPhrase);
+            }
+
+            throw new HttpRequestException(task.Result.ReasonPhrase);
+#else
+            return null;
+#endif
+        }
+
+        private Task<string> GetResponseAsync(string relativePath, CancellationToken cancellationToken)
+        {
+            var getStreamTask = GetStreamAsync(relativePath, cancellationToken);
+
+            return getStreamTask.ContinueWith(
+                task =>
+                {
+                    if (task.Status != TaskStatus.RanToCompletion)
+                        return string.Empty;
+                    using (var responseStream = task.Result)
+                    {
+                        return new StreamReader(responseStream).ReadToEnd();
+                    }
+                },
+                cancellationToken,
+                TaskContinuationOptions.AttachedToParent,
+                TaskScheduler.Current);
+        }
+        private Task<Stream> GetStreamAsync(string restServicePath, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            return _httpClient.GetAsync(restServicePath, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+                             .ContinueWith(
+                                 task => GetStreamFromHttpResponseAsync(task, restServicePath, cancellationToken),
+                                 cancellationToken,
+                                 TaskContinuationOptions.AttachedToParent,
+                                 TaskScheduler.Current)
+                             .Unwrap();
+        }
+
+        private DateTime TimestampToDateTime(long timestamp)
+        {
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTime.Now.Kind).AddMilliseconds(timestamp);
         }
 
         public void Dispose()
         {
-            if (_tfsHelper != null)
-                _tfsHelper.Dispose();
             GC.SuppressFinalize(this);
         }
     }
